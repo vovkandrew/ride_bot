93b9915b0cc957666f8c412aa59528db77f7d1ed
Serhii Yuzyna <serhiy.yuzyna@gmail.com>
2024-02-18 21:22:45 +0200

    trip-details-added-find-trip-handler-fixed-navigation-buttons

--- src/main/java/org/project/handler/trip/find/FindTripMenu.java
+++ src/main/java/org/project/handler/trip/find/FindTripMenu.java
@@ -1,13 +1,11 @@
 package org.project.handler.trip.find;
 
 import org.project.handler.UpdateHandler;
-import org.project.model.Phase;
-import org.project.model.Route;
-import org.project.model.Trip;
-import org.project.model.UserPhase;
+import org.project.model.*;
 import org.project.service.RouteService;
 import org.project.service.TripService;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
@@ -15,11 +13,12 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import java.util.Optional;
 
 import static java.lang.String.format;
+import static org.project.util.Keyboards.getAvailableCitiesKeyboard;
 import static org.project.util.Keyboards.getAvailableTripsForPassengerKeyboard;
-import static org.project.util.UpdateHelper.getUserIdFromUpdate;
-import static org.project.util.constants.Constants.DEFAULT_OFFSET;
-import static org.project.util.constants.Constants.DEFAULT_TRIP_LIMIT;
+import static org.project.util.UpdateHelper.*;
+import static org.project.util.constants.Constants.*;
 import static org.project.util.constants.Messages.FIND_TRIP_CHOOSE_TRIPS;
+import static org.project.util.constants.Messages.PROVIDE_CITY_TO;
 import static org.project.util.enums.HandlerName.*;
 import static org.springframework.data.domain.PageRequest.of;
 
@@ -43,11 +42,26 @@ public class FindTripMenu extends UpdateHandler {
         long userId = getUserIdFromUpdate(update);
         updateUserPhase(userPhase, handlerPhase);
         Route route = routeService.getNewPassengerRoute(userId);
-        deleteRemovableMessagesAndEraseAllFromRepo(userId);
+
         if (isMessageSentInsteadOfButtonClick(update)) {
             return;
         }
 
+        if (isUpdateContainsHandler(update, FIND_TRIP_MENU_NEXT)) {
+            int page = getOffsetParamFromUpdateByHandler(update, FIND_TRIP_MENU_NEXT);
+            PageRequest pageRequest = of(page, DEFAULT_TRIP_LIMIT);
+
+            deleteRemovableMessagesAndEraseAllFromRepo(userId);
+
+            Page<Trip> trips = tripService.findAllCreatedNonDriverTrips(route, pageRequest);
+
+            sendRemovableMessage(userId, format(FIND_TRIP_CHOOSE_TRIPS, route.getFormattedData()),
+                    getAvailableTripsForPassengerKeyboard(trips, FIND_TRIP_MENU_NEXT, FIND_TRIP_MENU_DETAILS));
+
+            return;
+        }
+
+        deleteRemovableMessagesAndEraseAllFromRepo(userId);
         Page<Trip> trips = tripService.findAllCreatedNonDriverTrips(route, of(DEFAULT_OFFSET, DEFAULT_TRIP_LIMIT));
         sendRemovableMessage(userId, format(FIND_TRIP_CHOOSE_TRIPS, route.getFormattedData()),
                 getAvailableTripsForPassengerKeyboard(trips, FIND_TRIP_MENU_NEXT, FIND_TRIP_MENU_DETAILS));
